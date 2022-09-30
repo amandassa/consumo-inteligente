a70f5a392e2e3eeb490d842619805f7a7706b4d6
Danrlei Almeida Araujo <danrleiaraujo@gmail.com>
2022-09-30 15:48:04 -0300

    Comentários

--- Problema1/API.py
+++ Problema1/API.py
@@ -1,21 +1,23 @@
+#importações
 import socket
 import threading
 import time
-import http.server 
+import http.server
 
+#Constantes:
 FORMATO = 'utf-8' 
-SERVER = "172.16.103.234"
+SERVER = "127.0.0.1"
 PROTOCOL= "HTTP/1.0"
 #--------------------------------------------------------------------------------
-#função que bloqueia ou desbloqueia hidrometro
+#função que bloqueia ou desbloqueia hidrometro através de comunicação TCP
 def funcionamento():
     #TCP - Funcionamento do hidrometro
     PORT = 8080 #Porta
     ADDR_S= (SERVER, PORT) #Endereço  
     while True:
         try:   
-            tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         #conexão TCP
-            tcp.connect(ADDR_S) 
+            tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         #inicia uma variavel como tcp
+            tcp.connect(ADDR_S) #conecta no servidor
             try:
                 n= 0;
                 while (n != 1):
@@ -30,28 +32,27 @@ def funcionamento():
                         n=1
                     else:
                         print("Opção Inválida")
-                tcp.send(enviar.encode(FORMATO))
+                tcp.send(enviar.encode(FORMATO)) #Comando para enviar dados
                 time.sleep(10)                                         #pausa para envio de dados
-                respostaSistema = tcp.recv(1024)
-                respostaSistema = respostaSistema.decode(FORMATO)
+                respostaSistema = tcp.recv(1024) #Resposta do sistema se foi recebido 
+                respostaSistema = respostaSistema.decode(FORMATO) #como vem em formato de byte, precisa de um decode
                 print("Recebido do servidor: ",respostaSistema)
             finally:
                 print('Fechando conexão...')
-                tcp.close()
+                tcp.close() #Fecha a conexão
         except:
             print("Servidor TCP desligado, tente novamente mais tarde.")
-            time.sleep(10)
-
+            time.sleep(10) #Tempo para uma nova tentativa
 #--------------------------------------------------------------------------------  
- 
 #função que recebe informações do hidrometro
 def recebeDados():
-    #UDP - Recebimento de dados
+    #UDP - Servidor para recebimento de dados
     PORT = 8090
-    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Inicia uma variavel com socket udp
     udp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    orig = (SERVER, PORT)
-    udp.bind(orig)
+    orig = (SERVER, PORT) #Endereço
+    udp.bind(orig) #Começa a ouvir
+    print("Ligado o Servidor UDP")
     try:
         while True:
             msg, cliente = udp.recvfrom(1024) #Recebe em bites a mensagem e o client
@@ -63,10 +64,10 @@ def recebeDados():
     finally:
         
         print ("Fechada a conexão com: ", cliente) 
-        udp.close()
+        udp.close() #Fecha a conexão udp
 #--------------------------------------------------------------------------------
  
-#função que recebe informações do hidrometro
+#função que inicia um servidor http no LocalHost
 def servidor():
     SERVERCLASS  = http.server.HTTPServer 
     HANDLERCLASS = http.server.SimpleHTTPRequestHandler 
