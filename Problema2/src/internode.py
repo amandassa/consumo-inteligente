e481266bf3ab61cee1ab994f833d813da13e5d81
Amanda <amandassa504@gmail.com>
2022-10-30 00:05:57 -0300

    serialização de mensagem,update banco de dados

--- Problema2/src/internode.py
+++ Problema2/src/internode.py
@@ -10,11 +10,12 @@ from concurrent.futures import thread
 from itertools import count
 import random
 import time
+from traceback import print_tb
 from paho.mqtt import client as mqtt_client
 import json
 from threading import Thread
 
-# infos dos hidrometros
+# infos dos hidrometros -------------------------------------
 '''
 Hidrômetros neste nó (db)
 Cada hidrômetro será uma entry no dicionário seguindo:
@@ -26,24 +27,23 @@ codigoH,
     bloqueado: boolean
 }
 '''
-hidrometros = {}
+hidroDB= {}
 
+#   brokers ---------------------------------------------------------------
 central = {
     'broker': 'localhost',      # mudar para maquina central do larsid
     'port': 1883,
-    'topicPub': "nuvem/nos",     # como gerar id para cada no criado?
-    'topicSub': "nuvem/nos/#"
+    'topicPub': "nuvem/",     # como gerar id para cada no criado?
+    'topicSub': "nuvem/#"
 }
 hidrometros = {
     'broker': 'localhost',  # mudar para maquina hidrometros do larsid
     'port': 1884,
-    'topicPub': "hidrometros/",     # como gerar id para cada no criado?
-    'topicSub': "hidrometros/#"
+    'topicPub': "hidrometros/bloqueio",
+    'topicSub': "hidrometros/status/#"
 }
-# username = 'emqx'
-# password = 'public'
-# generate client ID with pub prefix randomly
-# client_id = f'python-mqtt-{random.randint(0, 1000)}'
+# valores globais ------------------------------------------------------------
+LIMITE_CONSUMO = 200
 
 def connect_mqtt(broker, port, prefix):
     def on_connect(client, userdata, flags, rc):
@@ -59,69 +59,87 @@ def connect_mqtt(broker, port, prefix):
     
     return client
 
+# Salva um novo status do hidrometro no DB
 # cliente é a instacia mqtt criada na conexão com o broker que se quer ouvir
-def handleHidrometro(msg):     # o nó ouve do hidrometro
-    codigoH = msg.topic.split('/')[-1]
+def salvarHidrometro(msg):     # o nó ouve do hidrometro
+    codigoH = msg.topic.split('/')[-1]  # o codigo do hidrometro está no subtopico
     hidroData = json.loads(msg.payload.decode())     # converte a mensagem do hidrômetro em um dicionário
-    hidrometros[codigoH] = hidroData
+    if codigoH in hidroDB.keys():
+        hidroDB[codigoH].insert(0, hidroData)
+    else:
+        hidroDB[codigoH] = [hidroData]
+    return codigoH
 
 '''
-deve se inscrever no servidor central e tratar as mensagens de acordo com a finalidade
+deve se inscrever no servidor central e no servidor dos hidrometros
+e tratar as mensagens de acordo com a finalidade
 client: instancia mqtt criada no método connect_mqtt
-topicos e subtopicos da nuvem: 
-    /nos : mensagens da nuvem para os nós
-    /api : mensagens da api para a nuvem
 '''
 def subscribe (client, topico):
     def on_message(client, userdata, msg):
+        s = msg.payload.decode('utf-8')
+        print(f'MENSAGEM DO HIDROMETRO\n{s}')
         topico = msg.topic.split('/')
-        match topico[0]:
-            case 'hidrometros':
-                handleHidrometro(msg)
-                # TODO adicionar verificação de consumo para o bloqueio
-                print(f'hidrometros\t:\t{msg.payload.decode()}')
+        if (topico[0] == 'hidrometros'):
+            match topico[1]:
+                case 'status':
+                    print(f'MENSAGEM DO HIDROMETRO\t{msg.payload.decode()}')
+                    codigoH = salvarHidrometro(msg)
+                    if (hidroDB[codigoH][0]['consumo'] > LIMITE_CONSUMO):   # checa consumo + recente
+                        publish(client, f'{hidrometros["topicPub"]}/bloqueio/{codigoH}', '{"bloqueado":true}')
+                case _:
+                    pass
+        else:
+            match topico[1]:
+                case 'consumo':
+                    # deve enviar a lista com os 10 hidrometros de maior consumo daquele nó
+                    # TODO busca e ordenação no banco para os n hidrometros de maior consumo.
+                    
+                    publish(client, f'nuvem/consumo/{client._client_id}', 'lista10maiores')
+                case _:
+                    print("Case default")
 
-            case 'nuvem':
-                print(f'de nuvem para nos\t:\t{msg.payload.decode()}')
-                # deve enviar a lista com os 10 hidrometros de maior consumo daquele nó
-                # -- isso pode ser feito automaticamente periodicamente mas ainda não foi decidido.
-            case _:
-                print("Case default")
-
-    client.subscribe(topico)     # se inscreve no topico da nuvem referente aos nos.
+    client.subscribe(topico)
     client.on_message = on_message
 
 # publica automaticamente sua media parcial para a nuvem 
 # (client: cliente da nuvem)
 # esta funcao pode ser invocada numa thread para melhor desempenho
-def pubMedia(client):
-    total, count, media = 0, 0, 0
+# def pubMedia(client):
+#     total, count = 0, 0, 0
+#     while True:
+#         if (len(hidroDB.items())!=0){
+#             for k in hidroDB:
+#                 total += hidroDB.get(k).get('consumo')
+#                 count += 1
+#             media = total / count
+#             # topico media : nuvem/nos/media/:idnevoa
+#             client.publish(f'{central["topicPub"]}/{media}/{client._client_id.decode()}')
+#             time.sleep(5)
+
+#         }
+
+
+def publishTest(client, topic):
     while True:
-        for k in hidrometros:
-            total += hidrometros[k]['consumo']
-            count += 1
-        media = total / count
-        # topico media : nuvem/nos/media/:idnevoa
-        client.publish(f'{central["topicPub"]}/{media}/{client._client_id.decode()}')
-        time.sleep(5)
-    
+        client.publish(topic, f'\t{client._client_id.decode()}')
+        time.sleep(2)
 
 def publish(client, topic, msg):
-    client.publish(topic, f'{client._client_id.decode()}')
-    time.sleep(2)
+    client.publish(topic, msg)
     
 def run():
     # conexões com nuvem    ------------------------------------------ 
-    clientCentral = connect_mqtt(central['broker'], central['port'], 'n')
-    clientCentral.loop_start()
-    subscribe(clientCentral, central['topicSub'])
+    # clientCentral = connect_mqtt(central['broker'], central['port'], 'n')
+    # clientCentral.loop_start()
+    # subscribe(clientCentral, central['topicSub'])
     # conexões com hidrometros    ------------------------------------ 
     time.sleep(2)
     clientHidro = connect_mqtt(hidrometros['broker'], hidrometros['port'], 'h')
     clientHidro.loop_start()
-    print(clientHidro._client_id)
     subscribe(clientHidro, hidrometros['topicSub'])
-    pubMedia(clientCentral)
+    publishTest(clientHidro, hidrometros['topicPub'])
+    # pubMedia(clientCentral)
 
 if __name__ == "__main__":
     run()
