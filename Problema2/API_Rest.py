349bd5ff51b495a5c0b24f2944ceffa0604509a9
Danrlei Almeida Araujo <danrleiaraujo@gmail.com>
2022-11-01 08:19:25 -0300

    front

--- Problema2/API_Rest.py
+++ Problema2/API_Rest.py
@@ -1,24 +1,55 @@
 from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
-#route
-#function
-#template
-
-@app.route("/login", methods = ['GET', 'POST'])
-def login():
-    data = request.form
-    print (data)
-    return render_template ('login.html')
-
-@app.route("/GET", methods = ['GET'])
-def pagWeb():
-    return render_template ('pagWeb.html')
-
+#Pagina inicial
 @app.route("/",  methods = ['GET'])
 def index():
     return render_template ('index.html')
+
+#Página de login
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+    #Recebimento de dados:
+    usuario = request.form.get('usuario')
+    senha = request.form.get('senha')
+    #Verificação de ADM's:
+    with open('C:/Users/danrl/Desktop/MI-Redes/MI-Redes/Problema2/adm.json') as adm:
+        listaAdm = json.load(adm)
+        cont = 0
+        for c in listaAdm: 
+            cont += 1
+            if usuario == c['nome'] and senha == c ['senha']:
+                return render_template ('indexLogado.html')
+    #Verificação de usuarios:
+    with open('C:/Users/danrl/Desktop/MI-Redes/MI-Redes/Problema2/usuarios.json') as usuarios:
+        listaUsers = json.load(usuarios)
+        cont = 0
+        for c in listaUsers: 
+            cont += 1
+            if usuario == c['nome'] and senha == c ['senha']:
+                return 'Olá Usuario!'
+    #Caso contrário:
+    return render_template ('login.html')
+
+#Pagina incial depois de logado
+@app.route("/GET/", methods = ['GET'])
+def pagWeb():
+    return render_template ('indexLogado.html')
+
+#Pagina para verificação de hidrometros
+@app.route("/GET/Hidrometros", methods = ['GET', 'POST'])
+def hidrometros():
+    #Recebe a quantidade de hidrometros:
+    hidrometro = request.form.get('hidrometro')
+
+    #FALTA A PARTE DE MOSTRAR OS HIDROMETROS: 
+    # ---------------------------------------
+    # ---------------------------------------
+    
+    return render_template ('hidrometros.html')
+
 
 @app.route("/GET/<param>",  methods = ['GET'])   #requisicao para obter n hidrometros de maior consumo
 def maiorConsumo(param):
